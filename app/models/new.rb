5bc3b901ea3cecf20829984e873cc8dc92f18cb6
Vladimir <vladimir.gavrilov@hotmail.com>
2020-07-17 03:05:50 +0300

    New function add pictures to news items
    Now it is possible to add picture to the article from the index page.
    But it is not possible yet to add picture from Edit page.

--- app/models/new.rb
+++ app/models/new.rb
@@ -3,5 +3,6 @@ class New < ActiveRecord::Base
     validates :user_id, presence: true
     validates :title, presence: true, length: { minimum: 3, maximum: 120 }
     belongs_to :user
+    has_one_attached :picture
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :news, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :news, only: [:index, :show, :new, :create, :edit, :update, :destroy, :picture]
   root 'news#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
